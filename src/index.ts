eb5180ccc921df3eb56c0b942e72df45213ad431
khongchai <world1955@hotmail.com>
2021-01-24 20:03:23 +0700

    finished setting up MikroORM / first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+  //connect to database
+  const orm = await MikroORM.init(mikroConfig);
+
+  //run migration
+  await orm.getMigrator().up();
+
+  //run sql
+  /* const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post); */
+
+  /*   const posts = await orm.em.find(Post, {});
+  console.log(posts); */
+};
+
+main().catch((err) => console.error(err));
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,17 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.[jt]s$/, // regex pattern for the migration files
+  },
+  entities: [Post],
+  dbName: "redditclone",
+  type: "postgresql",
+  debug: !__prod__,
+  username: "postgres",
+  password: "postgres",
+} as Parameters<typeof MikroORM.init>[0]; //Parameters returns an array so we take only the first one
