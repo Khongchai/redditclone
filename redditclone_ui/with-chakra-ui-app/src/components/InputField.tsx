bd45d65099ac94f5fc3d83d7ecb406d0c0e292c9
khongchai <world1955@hotmail.com>
2021-01-29 17:36:28 +0700

    styled register a bit more

---
+++ redditclone_ui/with-chakra-ui-app/src/components/InputField.tsx
@@ -0,0 +1,38 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { useField } from "formik";
+import React, { InputHTMLAttributes } from "react";
+
+//this inputfield should have everything the normal input element has
+//plus some extra properties like everything after the ampersand
+//is no longer an optional field.
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  label: string;
+  placeholder: string;
+  name: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _,
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input
+        {...field}
+        {...props}
+        id={field.name}
+        placeholder={props.placeholder}
+      />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
--- redditclone_ui/with-chakra-ui-app/src/pages/register.tsx
+++ redditclone_ui/with-chakra-ui-app/src/pages/register.tsx
@@ -1,37 +1,55 @@
 import React from "react";
 import { Formik, Form } from "formik";
-import {
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
-} from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { Box, Button } from "@chakra-ui/react";
+import { useRegisterMutation } from "../generated/graphql";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+  //graphql code generator was brought in to the mutation
+  const [, register] = useRegisterMutation();
   return (
-    <Formik
-      initialValues={{ username: "", password: "" }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {({ values, handleChange }) => (
-        <Form>
-          <FormControl>
-            <FormLabel htmlFor="username">Username</FormLabel>
-            <Input
-              values={values}
-              onChange={handleChange}
-              id="username"
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values) => {
+          //the values in the initial values match exactly to the graphql
+          //variable values, so we can just pass in the values variable into
+          //register. Otherwise, it will have to be something like
+          //username: values.username
+          const response = await register(values);
+          return response;
+        }}
+      >
+        {(props) => (
+          <Form>
+            <InputField
+              name="username"
               placeholder="username"
+              label="Username"
             />
-            {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-          </FormControl>
-        </Form>
-      )}
-    </Formik>
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              isLoading={props.isSubmitting}
+              colorScheme="teal"
+              type="submit"
+            >
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
   );
 };
 
